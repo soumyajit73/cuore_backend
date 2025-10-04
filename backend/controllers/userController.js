935621a3c2f4357e1d6babad864508343ae0670d
Soumyajit <soumyajitdatta1234@gmail.com>
2025-10-04 09:44:09 +0530

    chnaged import of User 2

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user'); 
+const User = require('../models/User'); 
 const mongoose = require('mongoose'); // <-- MUST BE ADDED
 
 exports.updateProfile = async (req, res) => {
